72bf71f908dbc09d6016f7a886a4b298a7f5bfeb
Julián Silveira <silveiracarnotajulian@gmail.com>
2018-04-11 19:19:49 +0200

    proyecto

---
+++ Assets/CharacterAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface CharacterAction {
+    void Attack();
+    void Run();
+    void Walk();
+    void FollowPath();
+    void SetTarget();
+    void ReleaseTarget();
+    void ScapeFromTarget();
+}
