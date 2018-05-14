3f97f7d15721b66cfc395ad9a2dcb23e24b057aa
gilberto.plaza <gilberto.plaza@udc.es>
2018-05-14 20:24:26 +0200

    Reorganizadas carpetas

---
+++ Assets/Scripts/Sound/audioMenuBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EazyTools.SoundManager;
+using SoundCte;
+
+public class audioMenuBackground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SoundManager.PlayMusic (SoundCte.SoundCte.LoadAudioClip (SoundCte.SoundCte.MUSICA5_SOUND), 0.8f, true, true);
+	}
+
+	public void StopBackGround () {
+		SoundManager.StopAllMusic ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
