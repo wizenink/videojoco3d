13bb03626f4c5e647392472ca15dde9d45d77b9e
gilberto.plaza <gilberto.plaza@udc.es>
2018-05-15 23:52:33 +0200

    Menus

---
+++ Assets/Scripts/Level1Scripts/Level1Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using EazyTools.SoundManager;
+
+public class Level1Script : MonoBehaviour {
+
+	private Player player;
+	public bool paused = false;
+	// Use this for initialization
+	void Start () {
+		if (GameObject.Find ("EmptyPlayerCompleto") != null) {
+			player = GameObject.Find ("EmptyPlayerCompleto").GetComponent <Player> ();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!paused) {
+			Time.timeScale = 1;
+			player.enabled = true;
+			if (Input.GetKeyDown (KeyCode.Escape)) {
+				player.enabled = false;
+				SoundManager.StopAllSounds ();
+				SoundManager.globalMusicVolume = 0.3F;
+				Time.timeScale = 0;
+				paused = true;
+				SceneManager.LoadScene ("PauseMenu", LoadSceneMode.Additive);
+			}
+		}
+	}
+}
