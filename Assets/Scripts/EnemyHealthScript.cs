3ff413aa4a4a32dc813d3dd458490dba419b7250
Sr.Bruno <sr.brunocabado@gmail.com>
2018-05-16 18:37:38 +0200

    Añadidas barras de vida

---
+++ Assets/Scripts/EnemyHealthScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthScript : MonoBehaviour
+{
+    private Character player;
+    private Slider _slider;
+    // Use this for initialization
+    void Start()
+    {
+        player = transform.parent.parent.GetComponent<Character>();
+        Transform temp = transform.Find("EnemyHealthSlider");
+        if (temp != null)
+        {
+            _slider = temp.GetComponent<Slider>();
+            _slider.maxValue = player.HealthPoints;
+            _slider.value = player.HealthPoints;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _slider.value = player.HealthPoints;
+    }
+}
