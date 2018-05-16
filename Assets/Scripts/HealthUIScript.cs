6f0396bdba369f97f6d9efa8fde4f5ae39dd274c
David Maseda Neira <david.maseda@hotmail.com>
2018-05-16 18:21:12 +0200

    Added prefabs

---
+++ Assets/Scripts/HealthUIScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUIScript : MonoBehaviour {
+    public Character player;
+    private Slider _slider;
+	// Use this for initialization
+	void Start () {
+        GameObject temp = GameObject.Find("HealthSlider");
+        if(temp != null)
+        {
+            _slider = temp.GetComponent<Slider>();
+            _slider.maxValue = player.HealthPoints;
+            _slider.value = player.HealthPoints;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _slider.value = player.HealthPoints;
+	}
+}
